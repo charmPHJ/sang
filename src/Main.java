237feb1ab465a051aa7ff1fc2553d10337b263bc
charmPHJ <againsay77@naver.com>
2021-11-08 14:18:42 +0900

    second

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,56 @@
 
 public class Main {
     public static void main(String[] args) {
-        Elf elf = new Elf("엘프", 100);
-        System.out.println(elf.toString());
+        Person park = new Person("팍");
 
-        HighElf hi = new HighElf("하이엘프", 120, 200);
-        System.out.println(hi.toString());
+        Develop dev = park;
+        Teacher tea = park;
+        Back bac = park;
 
-        LoadElf lo = new LoadElf("로드엘프", 100, 140, 220);
-        System.out.println(lo.toString());
-
-
+        dev.programing("java");
+        tea.teach("과학");
+        bac.play("게임");
     }
 }
-class Elf{
-    protected String name;
-    protected int hp;
+interface Develop{
+    public void programing(String lang);
 
-    public Elf(String name, int hp){
+}
+
+interface Teacher {
+    public void teach(String subject);
+}
+
+interface Back {
+    public void play(String playball);
+
+}
+
+class Person implements Develop, Teacher, Back {
+    private String name;
+
+    public Person(String name) {
         this.name = name;
-        this.hp = hp;
-    }
-    public String toString() {
-        return String.format("[%s]의 체력 : %d",this.name, this.hp);
-    }
-}
-class HighElf extends Elf{
-    protected int mp;
 
-    public HighElf(String name, int hp, int mp){
-        super(name,hp);
-        this.mp = mp; // 이걸할려면 메소드 오버라이딩해야됨.
-    }
-    public String toString(){
-        return String.format("[%s]의 체력 : %d   마나: %s", super.name, super.hp, this.mp);
+
     }
 
-}
-class LoadElf extends HighElf{
-    protected int sh;
+    public void programing(String lang) {
+        System.out.printf("%s >> %s 프로그램중 \n", name, lang);
 
-    public LoadElf(String name, int hp, int mp, int sh){
-        super(name,hp,mp);
-        this.sh = sh;
     }
-    public String toString(){
-        return String.format("[%s]의 체력 : %d   마나: %s, 쉴드: %s", super.name, super.hp, super.mp,this.sh);
+
+    public void teach(String subject){
+        System.out.printf("%s >> %s 가르치는 중 \n", name, subject);
+
     }
+
+    public void play(String playball) {
+        System.out.printf("%s >> %s 노는 중 \n", name, playball);
+
+    }
+
 
 }
-//rmt
+
 
