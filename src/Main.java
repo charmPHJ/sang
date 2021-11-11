12adae0fff61007248c68bc323bb13ee6ea1fa18
charmPHJ <againsay77@naver.com>
2021-11-11 11:52:10 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,38 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+class StudentInfo {
+        public int grade;
+        StudentInfo(int grade) {
+            this.grade = grade;
+}
+}
+class EmployeeInfo {
+    public int rank;
+    EmployeeInfo(int rank) {
+        this.rank = rank;
+    }
+}
 
+class Person {
+        public Object info;
+        Person(Object info) {
+            this.info = info; // 여기 object는 공통적인 언어로 모든곳에 쓰임
+}
+}
 
 public class Main {
-    public static void main(String[] args) { // 여기 스코어와
-        Knight don = new Knight("돈키호테", 30);
-        System.out.printf("%s\n", don.toString());
+       public static void main(String[] args) { // 여기 스코어와
+                Person p1 = new Person("부장");  // object를 썼기때문에ㅐ 여기 어떠한 말을 써도 되는 오류가 나타남
+                EmployeeInfo ei = (EmployeeInfo)p1.info; // object 타입이라 괄호안에 형변화 시키는 변수를 넣는다 타입을 맞추기위해
+           System.out.println(ei.rank);
+
+            }
+
+        }
 
 
-        don.setHp(don.getHp() + 30) ;
-        System.out.printf("%s\n", don.toString());
-    }
-}
-class Knight {
-    private String name;
-    private int hp;
-
-    public Knight(String name, int hp) {
-        this.name = name;
-        this.hp = hp;
-
-    }
-
-    public int getHp(){
-        return this.hp;
-
-    }
-
-    public void setHp(int hp) {
-        this.hp = hp;
-
-    }
-
-    public String toString() {
-        return String.format("Knight { 나이트 이름 : %s, 체력 : %d }", name, hp);
-    }
-
-}
 
 
 
